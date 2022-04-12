370990926e034b598b2541864eec434c48e5f6a3
HyeokjinKang <coupy@croissant.tech>
2022-04-12 11:00:14 +0900

    서버-클라이언트간 자리 투표 통신을 위한 지원

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,26 @@ io.on("connection", (socket) => {
     io.to(id).emit("name-result", result);
   });
 
+  socket.on("seat-vote-start", (num) => {
+    socket.broadcast.emit("seat-vote-start", num);
+  });
+
+  socket.on("seat-vote", (n) => {
+    io.to(users.admin[0]).emit("seat-vote", n, socket.id);
+  });
+
+  socket.on("seat-voted", (id) => {
+    io.to(id).emit("seat-voted");
+  });
+
+  socket.on("seat-versus", (id, len) => {
+    io.to(id).emit("seat-versus", len);
+  });
+
+  socket.on("seat-confirm", (id) => {
+    io.to(id).emit("seat-confirm");
+  });
+
   socket.on("disconnect", () => {
     if (users.undefined.indexOf(socket.id) != -1) users.undefined.splice(users.undefined.indexOf(socket.id), 1);
     else if (users.student.indexOf(socket.id) != -1) {
